25b438295cba68053f3171e660d22c23a3350801
aaron blum <blumaa@gmail.com>
2019-10-18 10:39:19 -0500

    organized index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,44 @@ function main() {
     const URL = 'http://localhost:3000/beers'
     const beerListUl = document.getElementById('list-group')
     // console.log(beerListUl)
+
     getBeers()
+
+    // get all the beerzes LIST
+
+    function getBeers() {
+      fetch(URL)
+        .then(resp => resp.json())
+        .then(beersData => {
+          renderBeerList(beersData)
+        })
+    }
+
+    function renderBeerList(beersData) {
+      beerListUl.innerHTML = ""
+      const beerListLis = beersData.map(beer => {
+        return renderBeerListItem(beer)
+      }).join('')
+      // console.log(beerListLis)
+      beerListUl.innerHTML = beerListLis
+      // console.log(beerListUl)
+    }
+
+    function renderBeerListItem(beer) {
+      // console.log(beer)
+      return (`
+        <li class="list-group-item" id="${beer.id}">${beer.name}</li>
+        `)
+      // console.log(beerLi)
+    }
+
+    // beer list event listeners
 
     beerListUl.addEventListener('click', (event) => {
       // console.log(event.target)
       getBeer(event.target)
     })
+
 
     // get A beer
 
@@ -17,9 +49,8 @@ function main() {
       fetch(URL + `/${target.id}`)
         .then(resp => resp.json())
         .then(beerData => renderBeer(beerData))
-
-
     }
+
 
     // render one beer
 
@@ -62,45 +93,14 @@ function main() {
     // patch the data for single beer
 
     function runBeerPatch(beerData){
-      console.log(beerData.id)
-      console.log(beerData.description)
+      // console.log(beerData.id)
+      // console.log(beerData.description)
       reqObj = {
         method: 'PATCH',
         headers: {'Content-Type' : 'application/json'},
         body: JSON.stringify(beerData)
       }
       fetch(URL + `/${beerData.id}`, reqObj)
-    }
-
-
-
-    // get all the beerzes LIST
-
-    function getBeers() {
-      fetch(URL)
-        .then(resp => resp.json())
-        .then(beersData => {
-          renderBeerList(beersData)
-        })
-    }
-
-    function renderBeerList(beersData) {
-      beerListUl.innerHTML = ""
-      const beerListLis = beersData.map(beer => {
-        return renderBeerListItem(beer)
-      }).join('')
-      // console.log(beerListLis)
-      beerListUl.innerHTML = beerListLis
-      // console.log(beerListUl)
-    }
-
-    function renderBeerListItem(beer) {
-      // console.log(beer)
-      return (`
-        <li class="list-group-item" id="${beer.id}">${beer.name}</li>
-        `)
-      // console.log(beerLi)
-
     }
 
   })
